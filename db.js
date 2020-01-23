371bc844b00dce7c1426c6cbdab7443be6b8782d
Azra Balić <azrabalic@hotmail.com>
2020-01-23 02:16:53 +0100

    logging false

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("DBWT19", "root", "root", {
    host: "localhost",
-   dialect: "mysql"
+   dialect: "mysql",
+   logging:false
 });
 
 let db= {};
